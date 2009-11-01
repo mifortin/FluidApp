a722e5d4a7ffeed236b327b152e85a79671d4f57
Michael <michael.fortin@gmail.com>
2009-11-01 08:35:43 -0500

    Broken Build

---
+++ FluidFPSView.h
@@ -0,0 +1,13 @@
+//
+//  FluidFPSView.h
+//  FluidApp
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface FluidFPSView : NSView {
+
+}
+
+@end
---
+++ FluidFPSView.m
@@ -0,0 +1,26 @@
+//
+//  FluidFPSView.m
+//  FluidApp
+//
+//  Created by Michael Fortin on 09-11-01.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "FluidFPSView.h"
+
+
+@implementation FluidFPSView
+
+- (id)initWithFrame:(NSRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect {
+    // Drawing code here.
+}
+
+@end
